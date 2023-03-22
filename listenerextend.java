c29cc578975853c8547136144f1c53ac3f8fe62b
Abdullah <abdullah180463@feng.bu.edu.eg>
2023-03-22 20:56:55 +0200

    modify listener

--- listenerextend.java
+++ listenerextend.java
@@ -2,9 +2,32 @@ import org.antlr.v4.runtime.ParserRuleContext;
 
 public class listenerextend extends CompilerBaseListener{
 
+    int x=0;
+    static String data="";
+
+    @Override
+    public void enterMainBlock(CompilerParser.MainBlockContext ctx) {
+        super.enterMainBlock(ctx);
+        System.out.println("{"+"// Block number "+x);
+        data+="{"+"// Block number "+x+"\n";
+        x++;
+    }
+
+    @Override
+    public void exitMainBlock(CompilerParser.MainBlockContext ctx) {
+        super.exitMainBlock(ctx);
+        System.out.println("   } ");
+        data+="} "+"\n";
+
+    }
+
     @Override
     public void enterBlock(CompilerParser.BlockContext ctx) {
         super.enterBlock(ctx);
+        System.out.println("{"+"// Block number "+x);
+        data+="{"+"// Block number "+x+"\n";
+
+        x++;
 
        // System.out.print("//block");
     }
@@ -12,5 +35,11 @@ public class listenerextend extends CompilerBaseListener{
     @Override
     public void exitBlock(CompilerParser.BlockContext ctx) {
         super.exitBlock(ctx);
+        System.out.println("   } ");
+        data+="} "+"\n";
+
+
+
+
     }
 }
