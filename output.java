0ed302a4f9eaeeb989081bece7b480a0a4845c31
faroukaf <43047208+faroukaf@users.noreply.github.com>
2023-03-28 23:54:51 +0200

    output

---
+++ output.java
@@ -0,0 +1,28 @@
+import java.io.*;
+public class input {
+    public static void main(String[] args) throws Exception{//block number 1
+		File output = new File("output.txt");
+		output.createNewFile();
+		FileWriter w = new FileWriter("output.txt");
+		w.write("block 1 is Visited " +"\n");
+
+        int x=5;
+        if(x==5){//block number 2
+
+            System.out.println(x);
+        w.write("block 2 is Visited" +"\n");
+}
+        else{//block number 3
+
+            System.out.println("error");
+        w.write("block 3 is Visited" +"\n");
+}
+        for(int i=0;i<x;i++){//block number 4
+
+        w.write("block 4 is Visited" +"\n");
+}
+
+
+    w.close();
+}
+}
