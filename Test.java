9b7480762a89859cba9213b94267fa88cc980f27
faroukaf <43047208+faroukaf@users.noreply.github.com>
2023-04-19 19:38:03 +0200

    html handle

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        int[] Array;
+
         String inputFile = "input.java";
         FileInputStream inputStream = new FileInputStream(inputFile);
         ANTLRInputStream input = new ANTLRInputStream(inputStream);
@@ -15,12 +15,68 @@ public class Test {
         ParseTree tree = parser.compilationUnit();
         ParseTreeWalker walker = new ParseTreeWalker();
         TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
-        walker.walk(new listenerextend(rewriter), tree);
+        walker.walk(new addprakets(rewriter), tree);
 
-        File output = new File("output.java");
+        File output = new File("output1.java");
         output.createNewFile();
-        FileWriter w = new FileWriter("output.java");
+        FileWriter w = new FileWriter("output1.java");
         w.write(rewriter.getText());
         w.close();
+
+        //add prakets
+        inputFile = "output1.java";
+        inputStream = new FileInputStream(inputFile);
+        input = new ANTLRInputStream(inputStream);
+        lexer = new JavaLexer(input);
+        tokens = new CommonTokenStream(lexer);
+        parser = new JavaParser(tokens);
+        tree = parser.compilationUnit();
+        walker = new ParseTreeWalker();
+
+        rewriter = new TokenStreamRewriter(tokens);
+        //convert edit.java to out put.java that will produce text file having blocks visited
+        walker.walk(new listenerextend(rewriter), tree);
+         output = new File("output2.java");
+        output.createNewFile();
+        FileWriter wedit = new FileWriter("output2.java");
+        wedit.write(rewriter.getText());
+        wedit.close();
+
+//for run code
+       Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec("java output2.java");
+        try {
+            pr.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    //for HTML
+        //html file part
+
+        inputFile = "output1.java";
+        inputStream = new FileInputStream(inputFile);
+        input = new ANTLRInputStream(inputStream);
+        lexer = new JavaLexer(input);
+        tokens = new CommonTokenStream(lexer);
+        parser = new JavaParser(tokens);
+        tree = parser.compilationUnit();
+        walker = new ParseTreeWalker();
+        rewriter = new TokenStreamRewriter(tokens);
+
+        for (int i = 0; i < tokens.getTokens().size(); i++) {
+            Token token = tokens.getTokens().get(i);
+            if (token.getText().equals( "<")) {
+                rewriter.replace(token, "&lt;");
+            } else if (token.getText().equals(">")) {
+                rewriter.replace(token, "&gt;");
+            }
+        }
+        walker.walk(new listnerhtml(rewriter), tree);
+        File html = new File("output.html");
+        html.createNewFile();
+        FileWriter w2 = new FileWriter("output.html");
+        w2.write(rewriter.getText());
+        w2.close();
     }
 }
