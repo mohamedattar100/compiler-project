1d381d7b3f5fbef67c63bd5e99a6ecc6fb57b143
AliAhmed3 <66383534+AliAhmed3@users.noreply.github.com>
2023-04-19 19:32:55 +0200

    update listenerextend

--- listenerextend.java
+++ listenerextend.java
@@ -5,34 +5,84 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class listenerextend extends JavaParserBaseListener{
-    int counter;
+    int counter=0;
+    boolean term = false;
+    int count=0;
     TokenStreamRewriter rewriter;
     public listenerextend(TokenStreamRewriter rewriter){
         this.rewriter = rewriter;
         this.counter=0;
     }
-@Override public void enterBlock(JavaParser.BlockContext ctx) {
-    this.counter++;
+    @Override public void enterBlock(JavaParser.BlockContext ctx) {
+        this.counter++;
         rewriter.insertAfter(ctx.getStart(),"//block number " + this.counter+"\n");//write after {
-
         if(counter==1) {//in main
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "try{\n");
             rewriter.insertAfter(ctx.getStart(),"\t"+"\t"+"File output = new File(\"output.txt\");"+"\n");
             rewriter.insertAfter(ctx.getStart(),"\t"+"\t"+"output.createNewFile();"+"\n");
-            rewriter.insertAfter(ctx.getStart(),"\t"+"\t"+"FileWriter w = new FileWriter(\"output.txt\");"+"\n");
-            rewriter.insertAfter(ctx.getStart(),"\t"+"\t"+"w.write(\"block "+ this.counter + " is Visited \" +\"\\n\");"  +"\n");
+            rewriter.insertAfter(ctx.getStart(),"\t"+"\t"+"FileWriter w1 = new FileWriter(\"output.txt\");"+"\n");
+            rewriter.insertAfter(ctx.getStart(),"\t"+"\t"+"w1.write(\"block "+ this.counter + " is Visited\" +\"\\n\");"  +"\n");
+            //rewriter.insertAfter(ctx.getStop(),"w1.close();"+"\n");
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "w1.close();" + "\n");
 
-            rewriter.insertBefore(ctx.getStop(),"w.close();"+"\n");//the last }
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "}catch (IOException e) {throw new RuntimeException(e);}\n");
+
         }
-}
-}
-else{
-            rewriter.insertBefore(ctx.getStop(),"w.write(\"block "+ this.counter + " is Visited\" +\"\\n\");"  +"\n");
+        else{
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "try{\n");
+
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "FileWriter w" + this.counter + " = new FileWriter(\"output.txt\",true);" + "\n");
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "w" + this.counter + ".write(\"block " + this.counter + " is Visited\"+\"\\n\");" + "\n");
+
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "w" + this.counter + ".close();" + "\n");
+            rewriter.insertAfter(ctx.getStart(), "\t" + "\t" + "}catch (IOException e) {throw new RuntimeException(e);}\n");
+
         }
 
     }
-    @Override public void exitBlock(JavaParser.BlockContext ctx) {
+    @Override
+    public void enterCompilationUnit(JavaParser.CompilationUnitContext ctx) {
+        rewriter.insertBefore(ctx.getStart(), "import java.io.*;" + "\n");
+    }
+    @Override public void enterClassBody(JavaParser.ClassBodyContext ctx) {
+        rewriter.insertAfter(ctx.getStart(),"\n \n \t public static boolean check(int NoOfExpression) {\n" +
+                "\t"  +   " try{FileWriter w = new FileWriter(\"the_visited_exp.txt\",true);\n" +
+                "\t"  +"  w.write(\"exp\"+NoOfExpression+\"is visited\\n\");\n" +
+                "\t"  +" w.close();\n}" +
+                "\t"  +" catch(Exception e){}\n"+
+                "\t"  +" return false;\n" +
+                "\t"+"\t"  +" }\n");
 
     }
-    @Override public void visitTerminal(TerminalNode node) {
+    @Override
+    public void visitTerminal(TerminalNode node) {
+        if(node.getText().equals("||")||node.getText().equals("&&"))
+        {term=true;}
+    }
+    @Override
+    public void enterParExpression(JavaParser.ParExpressionContext ctx)
+    {term=true;}
+    @Override
+    public void enterExpression(JavaParser.ExpressionContext ctx) {
+        if(term&&ctx.AND()==null&&ctx.OR()==null){
+
+            count++;
+
+            rewriter.insertBefore(ctx.getStart(),"(check("+count+")||");
+
+            rewriter.insertAfter(ctx.getStop(),")");
+
+            term=false;
+
+            if(ctx.getText().charAt(0)=='(')term=true;
+        }
+    }
+
+
 
     }
+
+
+
+
+
